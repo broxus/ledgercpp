6a9dd5eb8e09e53cb54ca14449203327f93708e6
Alexey Pashinov <pashinov93@gmail.com>
2021-04-12 12:58:13 +0300

    Add library to communicate with Ledger Nano S/X

---
+++ ledgercpp/ledger.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ledgercpp/transport.hpp"
+
+namespace ledger {
+	class Ledger {
+		enum APDU : uint8_t {
+			CLA = 0xe0,
+			INS_GET_APP_CONFIGURATION = 0x01,
+			INS_GET_PUBLIC_KEY = 0x02,
+			INS_SIGN = 0x03,
+		};
+
+	public:
+		Ledger();
+		~Ledger();
+
+		Error open();
+
+		std::tuple<Error, std::vector<uint8_t>> get_public_key(uint32_t account, bool confirm = false);
+		std::tuple<Error, std::vector<uint8_t>> sign(uint32_t account, const std::vector<uint8_t>& msg);
+
+		void close();
+
+	private:
+		std::unique_ptr<Transport> transport_;
+	};
+}
